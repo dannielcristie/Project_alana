e7523994bad3a4bcd8b213748ddc81b1405ba773
Danniel <dany14012@gmail.com>
2019-05-16 20:48:24 +0000

    projeto2

---
+++ project2.py
@@ -0,0 +1,8 @@
+class Academia:
+   def __init__(self):
+      self.clientes = []
+   def Cadastro(self, name, phone, age):
+      self.clientes.append(name)
+      self.clientes.append(phone)
+      self.clientes.append(age)
+   def Avaliaçao(self, ):
