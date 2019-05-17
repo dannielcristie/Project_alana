17b9f47d2512739660a32708a94180da357930bf
Danniel_Cristie <dany14012@gmail.com>
2019-05-16 21:37:00 -0300

    updateproject2

--- project2.py
+++ project2.py
@@ -1,8 +1,22 @@
+#coding=utf-8
+
+
+
 class Academia:
-   def __init__(self):
-      self.clientes = []
-   def Cadastro(self, name, phone, age):
-      self.clientes.append(name)
-      self.clientes.append(phone)
-      self.clientes.append(age)
-   def Avaliaçao(self, ):
+	def __init__(self):
+		
+		self.clientes = {}
+
+   	def Cadastro(self,name,phone,age):
+
+   		self.clientes[self.name] = name
+   		self.clientes[self.name] = phone
+   		self.clientes[self.name] = age
+
+
+clie1 = Academia()
+
+clie1.Cadastro(danniel,981187186,age)
+
+
+print(clie1)
